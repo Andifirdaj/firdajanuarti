c3908fedd38547d484dea83d3912d3a381c2a100
Andifirdaj <127635923+Andifirdaj@users.noreply.github.com>
2023-03-29 22:26:28 +0800

    Add files via upload

---
+++ TestErr0069.java
@@ -0,0 +1,19 @@
+/*Nama  : Andi Firda Januarti
+Stambuk : 13020210069
+
+ public class TestErr20069{
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+for(int i=0; i<10;i++);
+tot += i;
+if(i<j);
+System.out.println(i);
+else
+System.out.println(j);
+do{
+j++;
+}while(j<10)
+while (j<10);{
+j++;
+};
+}
